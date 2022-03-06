d97ca13ae16ba756e4765040dbb8b5e9d6d85947
Donald J Ross <54287731+Fulgrim3@users.noreply.github.com>
2022-03-06 09:04:50 -0500

    enter

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,45 +1,50 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from matplotlib.pyplot import title
+#from matplotlib.pyplot import title
 
-
+#create comment here
 class User(AbstractUser):
-    watched_listing=models.M2M('Listing', related_name='watchers')
+    pass
+    #watched_listing=models.M2M('Listing', related_name='watchers')
 
-class Category(#todo):
-    id 
-    name 
+
+#bid keeps track of the user id, the amount of money bid on ther certain id, the item(listing) that was bid on, and the bidder's unique user information as a way to track   id 
+ #   amount 
+ #   listing 
+ #   bidder 
+class Bid():
     pass
 
-class Bid(#todo):
-    id 
-    amount 
-    listing 
-    bidder 
+#it's a catoary that tracks the ID, and the name associated with the type of catagory 
+class Category():
+    pass
+#listing is the main table that will branch from all other tables... id title
+#    description 
+#    seller 
+#    image_url 
+#    starting bid 
+#    is_active 
+#    category 
+#    timestamp 
+class Listing():
     pass
 
-class Listing(#todo):
-    id 
-    title
-    description 
-    seller 
-    image_url 
-    starting bid 
-    is_active 
-    category 
-    timestamp 
-    pass
-
+#id, watcher, and listing
 class Watchlist(ManyToMany.UserListing):
-    id
-    watcher
-    listing
     pass
 
-class Comment(#todo):
-    id
-    author
-    body
-    timestamp
-    listing
+
+#comment will track author, text created, a timestamp of the comment, and which listing the comment is associated with
+class Comment(models.Model):
+    #id =
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author")
+    body = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True, blank=True)
+    #listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="get_comments")
+
+    class Meta: 
+        ordering =['timestamp']
+    
+    def __str__(self):
+        return 'Comment {} by {}' .format(self.body, self.name)
     pass
